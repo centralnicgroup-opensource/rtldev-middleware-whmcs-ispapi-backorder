9f3fac0c1f62fff700dd1bf2c3902579d0f6d396
Anthony Schneider <anthonys@hexonet.net>
2017-08-16 06:42:23 +0000

    reviewed

--- crons/batch_test.php
+++ crons/batch_test.php
@@ -4,28 +4,13 @@ $cronname = "BATCH_TEST";
 require_once dirname(__FILE__)."/../../../../init.php";
 require_once dirname(__FILE__)."/../backend/api.php";
 
-//SEND INVOICE
-$createinvoice = array(
-        "COMMAND" => "QueryBackorderList",
-
-);
-// QueryPriceList
-
-// ActivateBackorder
-// "DOMAIN" => "a-anand.com",
-
-// "COMMAND" => "CreateInvoice",
-// "DOMAIN" => "a-charity.com",
-// "TYPE" => "FULL",
-// "BACKORDERID" => 307,
-
-
-
-
-$r = backorder_api_call($createinvoice);
-echo "list <pre>";
-print_r($r);
-echo "</pre>";
+// $command = array(
+//         "COMMAND" => "QueryBackorderList",
+// );
+// $r = backorder_api_call($command);
+// echo "<pre>";
+// print_r($r);
+// echo "</pre>";
 
 echo date("Y-m-d H:i:s")." $cronname done.\n";
 ?>
