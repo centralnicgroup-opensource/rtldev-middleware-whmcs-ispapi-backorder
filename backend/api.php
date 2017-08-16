bf4698dbf1b887e2baf8c4fa7a0208d71afe2f8c
Anthony Schneider <anthonys@hexonet.net>
2017-08-16 05:25:02 +0000

    reviewed

--- backend/api.php
+++ backend/api.php
@@ -3,264 +3,261 @@ date_default_timezone_set('UTC');
 require_once dirname(__FILE__).'/../../../../init.php';
 require_once dirname(__FILE__).'/idna_convert.class.php';
 require_once dirname(__FILE__)."/helper.php"; //HELPER WHICH CONTAINS HELPER FUNCTIONS
-
 use WHMCS\Database\Capsule;
-try {
-	//GET DPO CONNECTION
-   $pdo = Capsule::connection()->getPdo();
 
-   //############################
-   //HELPER FUNCTIONS
-   //############################
-   function logmessage($cronname, $status, $message, $query){
-	   global $pdo;
-	   $insert=$pdo->prepare("INSERT INTO backorder_logs(cron, date, status, message, query) VALUES(:cron, :date, :status, :message, :query)");
-	   $insert->execute(array(':cron' => $cronname, ':date' => date("Y-m-d H:i:s") , ':status' => $status, ':message' => $message, ':query' => $query));
-   }
+//############################
+//HELPER FUNCTIONS
+//############################
+function logmessage($cronname, $status, $message, $query){
+    try {
+        $pdo = Capsule::connection()->getPdo();
+        $insert_stmt = $pdo->prepare("INSERT INTO backorder_logs(cron, date, status, message, query) VALUES(:cron, :date, :status, :message, :query)");
+        $insert_stmt->execute(array(':cron' => $cronname, ':date' => date("Y-m-d H:i:s") , ':status' => $status, ':message' => $message, ':query' => $query));
+    } catch (\Exception $e) {
+        die($e->getMessage());
+    }
+}
 
-   //THIS FUNCTION CALLS OUR HEXONET API AND IS USED FOR CRONS AND IN THE BACKEND
-   function ispapi_api_call($command){
-   	require_once(dirname(__FILE__)."/../../../../includes/registrarfunctions.php");
+//THIS FUNCTION CALLS OUR HEXONET API AND IS USED FOR CRONS AND IN THE WHMCS ADMIN AREA
+function ispapi_api_call($command){
+    require_once(dirname(__FILE__)."/../../../../includes/registrarfunctions.php");
+    $higher_version_required_message = "The ISPAPI DomainCheck Module requires ISPAPI Registrar Module v1.0.15 or higher!";
 
-   	//CHECK IF THE ISPAPI REGISTRAR MODULE IS INSTALLED
-   	$error = false;
-   	$message = "";
-   	if(file_exists(dirname(__FILE__)."/../../../../modules/registrars/ispapi/ispapi.php")){
-   		$file = "ispapi";
-   		require_once(dirname(__FILE__)."/../../../../modules/registrars/".$file."/".$file.".php");
-   		$funcname = $file.'_GetISPAPIModuleVersion';
-   		if(function_exists($file.'_GetISPAPIModuleVersion')){
+	//CHECK IF THE ISPAPI REGISTRAR MODULE IS INSTALLED
+	$error = false;
+	$message = "";
+	if(file_exists(dirname(__FILE__)."/../../../../modules/registrars/ispapi/ispapi.php")){
+		$file = "ispapi";
+		require_once(dirname(__FILE__)."/../../../../modules/registrars/".$file."/".$file.".php");
+		$funcname = $file.'_GetISPAPIModuleVersion';
+		if(function_exists($file.'_GetISPAPIModuleVersion')){
+			$version = call_user_func($file.'_GetISPAPIModuleVersion');
+			//check if version = 1.0.15 or higher
+			if( version_compare($version, '1.0.15') >= 0 ){
+				//check authentication
+				$registrarconfigoptions = getregistrarconfigoptions($file);
+				$ispapi_config = ispapi_config($registrarconfigoptions);
+				$checkAuthenticationCommand = array(
+						"command" => "CheckAuthentication",
+						"subuser" => $ispapi_config["login"],
+						"password" => $ispapi_config["password"],
+				);
+				$checkAuthentication = ispapi_call($checkAuthenticationCommand, $ispapi_config);
+				if($checkAuthentication["CODE"] != "200"){
+					$error = true;
+					$message = "The \"".$file."\" registrar authentication failed! Please verify your registrar credentials and try again.";
+				}
+			}else{
+				$error = true;
+				$message = $higher_version_required_message;
+			}
+		}else{
+			$error = true;
+			$message = $higher_version_required_message;
+		}
+	}else{
+		$error = true;
+		$message = $higher_version_required_message;
+	}
 
-   			$version = call_user_func($file.'_GetISPAPIModuleVersion');
-   			//check if version = 1.0.15 or higher
-   			if( version_compare($version, '1.0.15') >= 0 ){
-   				//check authentication
-   				$registrarconfigoptions = getregistrarconfigoptions($file);
-   				$ispapi_config = ispapi_config($registrarconfigoptions);
-   				$checkAuthenticationCommand = array(
-   						"command" => "CheckAuthentication",
-   						"subuser" => $ispapi_config["login"],
-   						"password" => $ispapi_config["password"],
-   				);
-   				$checkAuthentication = ispapi_call($checkAuthenticationCommand, $ispapi_config);
+	$response = array();
+	if($error){
+		$response["CODE"] = 549;
+		$response["DESCRIPTION"] = $message;
+	}else{
+		$response = ispapi_call($command, $ispapi_config);
+	}
+	return $response;
+}
 
-   				if($checkAuthentication["CODE"] != "200"){
-   					$error = true;
-   					$message = "The \"".$file."\" registrar authentication failed! Please verify your registrar credentials and try again.";
-   				}
-   			}else{
-   				$error = true;
-   				$message = "The ISPAPI DomainCheck Module requires ISPAPI Registrar Module v1.0.15 or higher!";
-   			}
-   		}else{
-   			$error = true;
-   			$message = "The ISPAPI DomainCheck Module requires ISPAPI Registrar Module v1.0.15 or higher!";
-   		}
-   	}else{
-   		$error = true;
-   		$message = "The ISPAPI DomainCheck Module requires ISPAPI Registrar Module v1.0.15 or higher!";
-   	}
+//THIS FUNCTION CALLS OUR LOCAL API AND IS USED FOR CUSTOMER AND ADMIN
+function backorder_api_call($command) {
+	$time = microtime(true);
+	$ca = new WHMCS_ClientArea();
 
-   	$response = array();
-   	if($error){
-   		$response["CODE"] = 549;
-   		$response["DESCRIPTION"] = $message;
-   	}else{
-   		$response = ispapi_call($command, $ispapi_config);
-   	}
-   	return $response;
-   }
+	$userid = $ca->getUserID();
 
-   //THIS FUNCTION CALLS OUR LOCAL API AND IS USED FOR CUSTOMER AND ADMIN
-   function backorder_api_call($command) {
-   	$time = microtime(true);
-   	$ca = new WHMCS_ClientArea();
+	//CHECK IF ADMIN LOGGED IN AND AUTHORIZE PASSING USERID TO COMMAND
+	if(isset($_SESSION['adminid']) && $_SESSION['adminid'] > 0 && isset($command["USERID"])){
+		$userid = $command["USERID"];
+	}
 
-   	$userid = $ca->getUserID();
+	$dir = opendir(dirname(__FILE__)."/../api");
+	$files = array();
+	while ( ($file = readdir($dir)) !== false ) {
+		if ( preg_match('/^command\.(.*)\.php$/', $file, $m) ) {
+			$files[strtoupper($m[1])] = $file;
+		}
+	}
+	$c = strtoupper($command["COMMAND"]);
+	if ( isset($files[$c]) ) {
+		$response = include dirname(__FILE__)."/../api/".$files[$c];
+	}
 
-   	//CHECK IF ADMIN LOGGED IN AND AUTHORIZE PASSING USERID TO COMMAND
-   	if(isset($_SESSION['adminid']) && $_SESSION['adminid'] > 0 && isset($command["USERID"])){
-   		$userid = $command["USERID"];
-   	}
+	if(empty($response)){
+		$response["CODE"] = 500;
+		$response["DESCRIPTION"] = "Command invalid";
+	}else{
+		if ( !isset($response["QUEUETIME"]) ) {
+			$response["QUEUETIME"] = "0.000";
+		}
+		if ( !isset($response["RUNTIME"]) ) {
+			$response["RUNTIME"] = sprintf("%0.3f", microtime(true) - $time);
+		}
+	}
 
-   	$dir = opendir(dirname(__FILE__)."/../api");
-   	$files = array();
-   	while ( ($file = readdir($dir)) !== false ) {
-   		if ( preg_match('/^command\.(.*)\.php$/', $file, $m) ) {
-   			$files[strtoupper($m[1])] = $file;
-   		}
-   	}
-   	$c = strtoupper($command["COMMAND"]);
-   	if ( isset($files[$c]) ) {
-   		$response = include dirname(__FILE__)."/../api/".$files[$c];
-   	}
+	return $response;
+}
 
-   	if(empty($response)){
-   		$response["CODE"] = 500;
-   		$response["DESCRIPTION"] = "Command invalid";
-   	}else{
-   		if ( !isset($response["QUEUETIME"]) ) {
-   			$response["QUEUETIME"] = "0.000";
-   		}
-   		if ( !isset($response["RUNTIME"]) ) {
-   			$response["RUNTIME"] = sprintf("%0.3f", microtime(true) - $time);
-   		}
-   	}
+//THIS FUNCTION CALLS OUR LOCAL API AND IS USED FOR CRONS AND SOME OTHER COMMANDS
+//$userid WILL TAKE THE VALUE OF $command["USER"]
+//THIS COMMAND IS NOT AVAILABLE FROM OUTSIDE
+function backorder_backend_api_call($command) {
+	$time = microtime(true);
+	$ca = new WHMCS_ClientArea();
 
-   	return $response;
-   }
+	$userid = $command["USER"];
+	$dir = opendir(dirname(__FILE__)."/../api");
+	$files = array();
+	while ( ($file = readdir($dir)) !== false ) {
+		if ( preg_match('/^command\.(.*)\.php$/', $file, $m) ) {
+			$files[strtoupper($m[1])] = $file;
+		}
+	}
+	$c = strtoupper($command["COMMAND"]);
+	if ( isset($files[$c]) ) {
+		$response = include dirname(__FILE__)."/../api/".$files[$c];
+	}
 
-   //THIS FUNCTION CALLS OUR LOCAL API AND IS USED FOR CRONS AND SOME OTHER COMMANDS
-   //$userid WILL TAKE THE VALUE OF $command["USER"]
-   //THIS COMMAND IS NOT AVAILABLE FROM OUTSIDE
-   function backorder_backend_api_call($command) {
-   	$time = microtime(true);
-   	$ca = new WHMCS_ClientArea();
+	if(empty($response)){
+		$response["CODE"] = 500;
+		$response["DESCRIPTION"] = "Command invalid";
+	}else{
+		if ( !isset($response["QUEUETIME"]) ) {
+			$response["QUEUETIME"] = "0.000";
+		}
+		if ( !isset($response["RUNTIME"]) ) {
+			$response["RUNTIME"] = sprintf("%0.3f", microtime(true) - $time);
+		}
+	}
+	return $response;
+}
 
-   	$userid = $command["USER"];
-   	$dir = opendir(dirname(__FILE__)."/../api");
-   	$files = array();
-   	while ( ($file = readdir($dir)) !== false ) {
-   		if ( preg_match('/^command\.(.*)\.php$/', $file, $m) ) {
-   			$files[strtoupper($m[1])] = $file;
-   		}
-   	}
-   	$c = strtoupper($command["COMMAND"]);
-   	if ( isset($files[$c]) ) {
-   		$response = include dirname(__FILE__)."/../api/".$files[$c];
-   	}
+//THIS FUNCTION IS USED FOR LISTINGS
+function backorder_api_query_list($command, $config = "") {
+	$response = backorder_api_call($command, $config);
 
-   	if(empty($response)){
-   		$response["CODE"] = 500;
-   		$response["DESCRIPTION"] = "Command invalid";
-   	}else{
-   		if ( !isset($response["QUEUETIME"]) ) {
-   			$response["QUEUETIME"] = "0.000";
-   		}
-   		if ( !isset($response["RUNTIME"]) ) {
-   			$response["RUNTIME"] = sprintf("%0.3f", microtime(true) - $time);
-   		}
-   	}
-   	return $response;
-   }
+	$list = array(
+			"CODE" => $response["CODE"],
+			"DESCRIPTION" => $response["DESCRIPTION"],
+			"RUNTIME" => $response["RUNTIME"],
+			"QUEUETIME" => $response["QUEUETIME"],
+			"ITEMS" => array()
+	);
+	foreach ( $response["PROPERTY"] as $property => $values ) {
+		if ( preg_match('/^(FIRST|LAST|COUNT|LIMIT|TOTAL|ITEMS|COLUMN)$/', $property) ) {
+			$list[$property] = $response["PROPERTY"][$property][0];
+		}
+		else {
+			foreach ( $values as $index => $value ) {
+				$list["ITEMS"][$index][$property] = $value;
+			}
+		}
+	}
 
-   //THIS FUNCTION IS USED FOR LISTINGS
-   function backorder_api_query_list($command, $config = "") {
-   	$response = backorder_api_call($command, $config);
+	if ( isset($command["FIRST"]) && !isset($list["FIRST"]) ) {
+		$list["FIRST"] = $command["FIRST"];
+	}
 
-   	$list = array(
-   			"CODE" => $response["CODE"],
-   			"DESCRIPTION" => $response["DESCRIPTION"],
-   			"RUNTIME" => $response["RUNTIME"],
-   			"QUEUETIME" => $response["QUEUETIME"],
-   			"ITEMS" => array()
-   	);
-   	foreach ( $response["PROPERTY"] as $property => $values ) {
-   		if ( preg_match('/^(FIRST|LAST|COUNT|LIMIT|TOTAL|ITEMS|COLUMN)$/', $property) ) {
-   			$list[$property] = $response["PROPERTY"][$property][0];
-   		}
-   		else {
-   			foreach ( $values as $index => $value ) {
-   				$list["ITEMS"][$index][$property] = $value;
-   			}
-   		}
-   	}
+	if ( isset($command["LIMIT"]) && !isset($list["LIMIT"]) ) {
+		$list["LIMIT"] = $command["LIMIT"];
+	}
 
-   	if ( isset($command["FIRST"]) && !isset($list["FIRST"]) ) {
-   		$list["FIRST"] = $command["FIRST"];
-   	}
+	if ( isset($list["FIRST"]) && isset($list["LIMIT"]) ) {
+		$list["PAGE"] = floor($list["FIRST"] / $list["LIMIT"]) + 1;
+		if ( $list["PAGE"] > 1 ) {
+			$list["PREVPAGE"] = $list["PAGE"] - 1;
+			$list["PREVPAGEFIRST"] = ($list["PREVPAGE"]-1) * $list["LIMIT"];
+		}
+		$list["NEXTPAGE"] = $list["PAGE"] + 1;
+		$list["NEXTPAGEFIRST"] = ($list["NEXTPAGE"]-1) * $list["LIMIT"];
+	}
+	if ( isset($list["TOTAL"]) && isset($list["LIMIT"]) ) {
+		$list["PAGES"] = floor(($list["TOTAL"] + $list["LIMIT"] - 1) / $list["LIMIT"]);
+		$list["LASTPAGEFIRST"] = ($list["PAGES"] - 1) * $list["LIMIT"];
+		if ( isset($list["NEXTPAGE"]) && ($list["NEXTPAGE"] > $list["PAGES"]) ) {
+			unset($list["NEXTPAGE"]);
+			unset($list["NEXTPAGEFIRST"]);
+		}
+	}
 
-   	if ( isset($command["LIMIT"]) && !isset($list["LIMIT"]) ) {
-   		$list["LIMIT"] = $command["LIMIT"];
-   	}
+	if ( !isset($list["COUNT"]) ) {
+		$list["COUNT"] = count($list["ITEMS"]);
+	}
 
-   	if ( isset($list["FIRST"]) && isset($list["LIMIT"]) ) {
-   		$list["PAGE"] = floor($list["FIRST"] / $list["LIMIT"]) + 1;
-   		if ( $list["PAGE"] > 1 ) {
-   			$list["PREVPAGE"] = $list["PAGE"] - 1;
-   			$list["PREVPAGEFIRST"] = ($list["PREVPAGE"]-1) * $list["LIMIT"];
-   		}
-   		$list["NEXTPAGE"] = $list["PAGE"] + 1;
-   		$list["NEXTPAGEFIRST"] = ($list["NEXTPAGE"]-1) * $list["LIMIT"];
-   	}
-   	if ( isset($list["TOTAL"]) && isset($list["LIMIT"]) ) {
-   		$list["PAGES"] = floor(($list["TOTAL"] + $list["LIMIT"] - 1) / $list["LIMIT"]);
-   		$list["LASTPAGEFIRST"] = ($list["PAGES"] - 1) * $list["LIMIT"];
-   		if ( isset($list["NEXTPAGE"]) && ($list["NEXTPAGE"] > $list["PAGES"]) ) {
-   			unset($list["NEXTPAGE"]);
-   			unset($list["NEXTPAGEFIRST"]);
-   		}
-   	}
+	if ( isset($list["FIRST"]) && !isset($list["LAST"]) ) {
+		$list["LAST"] = $list["FIRST"] + count($list["ITEMS"]) - 1;
+	}
 
-   	if ( !isset($list["COUNT"]) ) {
-   		$list["COUNT"] = count($list["ITEMS"]);
-   	}
+	return $list;
+}
 
-   	if ( isset($list["FIRST"]) && !isset($list["LAST"]) ) {
-   		$list["LAST"] = $list["FIRST"] + count($list["ITEMS"]) - 1;
-   	}
+// CREATE AN API RESPONSE
+function backorder_api_response($code, $info = "") {
+	$r = array("CODE" => $code, "DESCRIPTION" => "Error", "PROPERTY" => array());
+	$codes = array(
+			"200" => "Command completed successfully",
+			"504" => "Missing required attribute",
+			"505" => "Invalid attribute value syntax",
+			"540" => "Attribute value is not unique",
+			"541" => "Invalid attribute value",
+			"545" => "Entity reference not found",
+			"549" => "Command failed",
+	);
+	if ( isset($codes[$code]) ) {
+		$r["DESCRIPTION"] = $codes[$code];
+	}
+	if ( strlen($info) ) {
+		$r["DESCRIPTION"] .= "; ".$info;
+	}
+	return $r;
+}
 
-   	return $list;
-   }
+//CHECK THE DOMAIN SYNTAX
+function backorder_api_check_syntax_domain($domain) {
+	$IDN = new idna_convert();
+	if ( strlen($domain) > 223) return false;
+	if ( !preg_match('/^([a-z0-9](\-*[a-z0-9])*)(\.([a-z0-9](\-*[a-z0-9]+)*))+$/i', $IDN->encode($domain)) ) return false;
+	return true;
+}
 
-   // CREATE AN API RESPONSE
-   function backorder_api_response($code, $info = "") {
-   	$r = array("CODE" => $code, "DESCRIPTION" => "Error", "PROPERTY" => array());
-   	$codes = array(
-   			"200" => "Command completed successfully",
-   			"504" => "Missing required attribute",
-   			"505" => "Invalid attribute value syntax",
-   			"540" => "Attribute value is not unique",
-   			"541" => "Invalid attribute value",
-   			"545" => "Entity reference not found",
-   			"549" => "Command failed",
-   	);
-   	if ( isset($codes[$code]) ) {
-   		$r["DESCRIPTION"] = $codes[$code];
-   	}
-   	if ( strlen($info) ) {
-   		$r["DESCRIPTION"] .= "; ".$info;
-   	}
-   	return $r;
-   }
+//CHECK IF TLD IN THE PRICELIST
+function backorder_api_check_valid_tld($domain, $userid) {
+    try {
+        $pdo = Capsule::connection()->getPdo();
+        $IDN = new idna_convert();
+        $currencyid = NULL;
 
-   //CHECK THE DOMAIN SYNTAX
-   function backorder_api_check_syntax_domain($domain) {
-   	$IDN = new idna_convert();
-   	if ( strlen($domain) > 223) return false;
-   	if ( !preg_match('/^([a-z0-9](\-*[a-z0-9])*)(\.([a-z0-9](\-*[a-z0-9]+)*))+$/i', $IDN->encode($domain)) ) return false;
-   	return true;
-   }
-
-   //CHECK IF TLD IN THE PRICELIST
-   function backorder_api_check_valid_tld($domain, $userid) {
-       global $pdo;
-       $IDN = new idna_convert();
-       $currencyid=NULL;
-
-       $result=$pdo->prepare("SELECT currency FROM tblclients WHERE id=?");
-       $result->execute(array($userid));
-       $data = $result->fetch(PDO::FETCH_ASSOC);
-       if ( $data ) {
-           $currencyid= $data["currency"];
-       }
-       $tlds = "";
-       $result=$pdo->prepare("SELECT extension FROM backorder_pricing WHERE currency_id=?");
-       $result->execute(array($currencyid));
-       $data = $result->fetchAll(PDO::FETCH_ASSOC);
-       foreach ($data as $key => $value) {
+        $stmt = $pdo->prepare("SELECT currency FROM tblclients WHERE id=?");
+        $stmt->execute(array($userid));
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ( $data ) {
+           $currencyid = $data["currency"];
+        }
+        $tlds = "";
+        $stmt = $pdo->prepare("SELECT extension FROM backorder_pricing WHERE currency_id=?");
+        $stmt->execute(array($currencyid));
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($data as $value) {
            $tlds .= "|.".$value["extension"];
-       }
-
-       $tld_list = substr($tlds, 1);
-
-       if ( !preg_match('/^([a-z0-9](\-*[a-z0-9])*)\\'.$tld_list.'$/i', $IDN->encode($domain)) ) return false;
+        }
+        $tld_list = substr($tlds, 1);
+        if ( !preg_match('/^([a-z0-9](\-*[a-z0-9])*)\\'.$tld_list.'$/i', $IDN->encode($domain)) )
+            return false;
         return true;
-  }
-
-} catch (\Exception $e) {
-   logmessage("api", "DB error", $e->getMessage());
-   return backorder_api_response(599, "COMMAND FAILED. Please contact Support.");
+    } catch (\Exception $e) {
+        die($e->getMessage());
+    }
 }
 
 ?>
