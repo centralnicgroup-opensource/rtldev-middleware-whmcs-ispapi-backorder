65384f9aeec2505ce29dcd6d33b76b006cb8e3d9
Anthony Schneider <anthonys@hexonet.net>
2017-08-31 12:40:09 +0000

    optimized daily_dropdate_sync

--- crons/daily_dropdate_synch.php
+++ crons/daily_dropdate_synch.php
@@ -8,7 +8,7 @@ require_once dirname(__FILE__)."/../backend/api.php";
 use WHMCS\Database\Capsule;
 try{
 	$pdo = Capsule::connection()->getPdo();
-	$stmt = $pdo->prepare("SELECT * FROM backorder_domains");
+	$stmt = $pdo->prepare("SELECT * FROM backorder_domains WHERE status in ('REQUESTED','ACTIVE','PROCESSING')");
 	$stmt->execute();
 	$locals = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -16,15 +16,24 @@ try{
 		$stmt = $pdo->prepare("SELECT domain, zone, drop_date FROM backorder_pending_domains WHERE domain=? and zone=? limit 1");
    		$stmt->execute(array($local["domain"],$local["tld"]));
    		$online = $stmt->fetch(PDO::FETCH_ASSOC);
+		if($stmt->rowCount() != 0){
+			if($local["dropdate"] != $online["drop_date"] && $online["drop_date"] > date("Y-m-d H:i:s")) {
+				$old_dropdate = $local["dropdate"];
+				$new_dropdate = $online["drop_date"];
 
-		if($local["dropdate"] != $online["drop_date"]) {
+				$update_stmt = $pdo->prepare("UPDATE backorder_domains SET dropdate=?, updateddate=NOW() WHERE domain=? AND tld=?");
+				$update_stmt->execute(array($online["drop_date"], $local["domain"], $local["tld"]));
+				if($update_stmt->rowCount() != 0){
+					$message = "DROPDATE OF BACKORDER ".$local["domain"].".".$local["tld"]." (backorderid=".$local["id"].") SYNCHRONIZED ($old_dropdate => $new_dropdate)";
+					logmessage($cronname, "ok", $message);
+				}
+			}
+		}else{
 			$old_dropdate = $local["dropdate"];
-			$new_dropdate = $online["drop_date"];
-
-			$update_stmt = $pdo->prepare("UPDATE backorder_domains SET dropdate=?, updateddate=NOW() WHERE domain=? AND tld=?");
-			$update_stmt->execute(array($online["drop_date"], $local["domain"], $local["tld"]));
+			$update_stmt = $pdo->prepare("UPDATE backorder_domains SET dropdate='0000-00-00 00:00:00', updateddate=NOW() WHERE domain=? AND tld=? AND dropdate!='0000-00-00 00:00:00'");
+			$update_stmt->execute(array($local["domain"], $local["tld"]));
 			if($update_stmt->rowCount() != 0){
-				$message = "DROPDATE OF BACKORDER ".$local["domain"].".".$local["tld"]." (backorderid=".$local["id"].") SYNCHRONIZED ($old_dropdate => $new_dropdate)";
+				$message = "DROPDATE OF BACKORDER ".$local["domain"].".".$local["tld"]." (backorderid=".$local["id"].") SYNCHRONIZED ($old_dropdate => '0000-00-00 00:00:00') DOMAIN NO LONGER IN RGP";
 				logmessage($cronname, "ok", $message);
 			}
 		}
